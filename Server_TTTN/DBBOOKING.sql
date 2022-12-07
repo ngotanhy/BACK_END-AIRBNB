76790becbd21eb5e4e9c8ad138c1e3a10cc489c2
Hy Ngo <ngotanhy123456@gamil.com>
2022-12-07 11:35:10 +0700

    add db and postman

---
+++ Server_TTTN/DBBOOKING.sql
@@ -0,0 +1,146 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE TABLE `BookingRoom` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `checkIn` datetime NOT NULL,
+  `checkOut` datetime NOT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `customer_id` int NOT NULL,
+  `room_id` int NOT NULL,
+  `numberCustomer` int NOT NULL,
+  `adult` int DEFAULT NULL,
+  `child` int DEFAULT NULL,
+  `baby` int DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  `unit` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `room_id` (`room_id`),
+  CONSTRAINT `BookingRoom_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `Users` (`id`),
+  CONSTRAINT `BookingRoom_ibfk_2` FOREIGN KEY (`room_id`) REFERENCES `Room` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `Comments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `room_id` int DEFAULT NULL,
+  `customer_id` int DEFAULT NULL,
+  `datacomment` varchar(255) DEFAULT NULL,
+  `rate` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `customercomment_id` (`customer_id`),
+  KEY `idRoom` (`room_id`),
+  CONSTRAINT `Comments_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `Users` (`id`),
+  CONSTRAINT `Comments_ibfk_2` FOREIGN KEY (`room_id`) REFERENCES `Room` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `Image` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `room_id` int NOT NULL,
+  `urlimage` longtext NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `room_id` (`room_id`),
+  CONSTRAINT `Image_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `Room` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `Location` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `location` varchar(255) NOT NULL,
+  `provine` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `nation` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `image` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `Messages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user1` int NOT NULL,
+  `user2` int NOT NULL,
+  `message` varchar(1000) NOT NULL,
+  `send_id` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `send_id` (`send_id`),
+  CONSTRAINT `Messages_ibfk_1` FOREIGN KEY (`send_id`) REFERENCES `Users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `Room` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nameRoom` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  `numberCustomer` int DEFAULT NULL,
+  `bedRoom` int DEFAULT NULL,
+  `bed` int DEFAULT NULL,
+  `bathRoom` int DEFAULT NULL,
+  `washing` tinyint(1) DEFAULT NULL,
+  `tivi` tinyint(1) DEFAULT NULL,
+  `iron` tinyint(1) DEFAULT NULL,
+  `airCondition` tinyint(1) DEFAULT NULL,
+  `wifi` tinyint(1) DEFAULT NULL,
+  `kitchen` tinyint(1) DEFAULT NULL,
+  `parkingCar` tinyint(1) DEFAULT NULL,
+  `pool` tinyint(1) DEFAULT NULL,
+  `location_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `location_id` (`location_id`),
+  CONSTRAINT `Room_ibfk_1` FOREIGN KEY (`location_id`) REFERENCES `Location` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `Typeroom` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `room_id` int NOT NULL,
+  `typeroom` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `room_id` (`room_id`),
+  CONSTRAINT `Typeroom_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `Room` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `Users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `gender` tinyint(1) NOT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `role` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `birthday` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `avatar` longtext,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `password` (`password`)
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
